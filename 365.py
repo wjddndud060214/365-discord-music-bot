5d909cb225d504a44ccf5d7d5e61041e13a1ebb0
wjddndud060214 <111739918+wjddndud060214@users.noreply.github.com>
2023-04-20 21:01:24 +0900

    Update 365.py

--- 365.py
+++ 365.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands
 import youtube_dl
-import os
 
-TOKEN = 'MTA5ODU2MTk4NDY0OTIzNjUwMQ.GW98Ea.AFScV1koerez7B411deacUS00jE0dBBi_W2GXA'  # 디스코드 봇의 토큰
+TOKEN = ('MTA5ODU2MTk4NDY0OTIzNjUwMQ.GW98Ea.AFScV1koerez7B411deacUS00jE0dBBi_W2GXA')
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -23,5 +22,5 @@ async def play(ctx, *, search):
     player = await voice_client.create_ytdl_player(url)
     player.start()
 
-access_token = os.environ["BOT_TOKEN"]
-bot.run(access_token)
+
+bot.run('MTA5ODU2MTk4NDY0OTIzNjUwMQ.GW98Ea.AFScV1koerez7B411deacUS00jE0dBBi_W2GXA')
