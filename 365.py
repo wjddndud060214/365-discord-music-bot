d1f733c958092b1b1e4df543e8a095f29ab48646
wjddndud060214 <111739918+wjddndud060214@users.noreply.github.com>
2023-04-20 20:31:17 +0900

    Add files via upload

---
+++ 365.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+TOKEN = 'MTA5ODU2MTk4NDY0OTIzNjUwMQ.GW98Ea.AFScV1koerez7B411deacUS00jE0dBBi_W2GXA'  # 디스코드 봇의 토큰
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name='play', name='p')
+async def play(ctx, *, search):
+    query = f'ytsearch:{search}'
+    voice_channel = ctx.author.voice.channel
+
+    with youtube_dl.YoutubeDL() as ydl:
+        info = ydl.extract_info(query, download=False)
+        url = info['entries'][0]['url']
+
+    voice_client = await voice_channel.connect()
+    player = await voice_client.create_ytdl_player(url)
+    player.start()
+
+bot.run(MTA5ODU2MTk4NDY0OTIzNjUwMQ.GW98Ea.AFScV1koerez7B411deacUS00jE0dBBi_W2GXA)
